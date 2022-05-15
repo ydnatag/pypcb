bf9f6c686e56d879996c8a3471bc55552338bd26
Andres Demski <andresdemski@gmail.com>
2022-05-15 15:34:08 -0300

    setup.py: initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypcb",
+    version="0.1",
+    author="ademski",
+    email="andresdemski@gmail.com",
+    packages=find_packages(),
+)
